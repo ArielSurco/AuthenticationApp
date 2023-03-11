e4e4e26c9184cab63fcb5ca2af3ebfc7f251b02d
ArielSurco <arielsurcoa@gmail.com>
2023-03-11 03:29:45 -0300

    Create Card atom

---
+++ client/src/components/ui/atoms/Card/Card.tsx
@@ -0,0 +1,18 @@
+// Internal
+import { concatClassnames } from 'utils/functions';
+
+// Styles
+import './Card.scss';
+
+interface ICardProps {
+  children: JSX.Element | JSX.Element[];
+  className?: string;
+}
+
+const Card = ({ children, className }: ICardProps) => (
+  <div className={concatClassnames(className, 'card')}>
+    {children}
+  </div>
+);
+
+export default Card;
