3a05db3657fa500542c667a4a88fd4b4cc80a331
ArielSurco <arielsurcoa@gmail.com>
2023-03-11 20:55:42 -0300

    Modify input atom

--- client/src/components/ui/atoms/Input/Input.tsx
+++ client/src/components/ui/atoms/Input/Input.tsx
@@ -8,18 +8,24 @@ import { concatClassnames } from 'utils/functions';
 import './Input.scss';
 
 interface IInputProps extends HTMLAttributes<HTMLInputElement> {
+  label?: string;
   icon?: JSX.Element | null;
   isTextArea?: boolean;
 }
 
-const Input = ({ className, icon = null, ...rest }: IInputProps) => (
-  <div className={concatClassnames(className, 'input-container')}>
-    {icon && (
-    <span className="icon-container">
-      {icon}
-    </span>
-    )}
-    <input className="input" {...rest} />
+const Input = ({
+  className, label = '', icon = null, ...rest
+}: IInputProps) => (
+  <div className={concatClassnames(className, 'input-field')}>
+    {label && <label htmlFor={rest.id} className="label">{label}</label>}
+    <div className="input-container">
+      {icon && (
+      <span className="icon-container">
+        {icon}
+      </span>
+      )}
+      <input className="input" {...rest} />
+    </div>
   </div>
 );
 
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -14,7 +14,7 @@ const Login = () => (
     <img src={Logo} alt="logo" />
     <Title level="h1" fontWeight={600}>Join thousands of learners from around the world</Title>
     <Title level="h2" fontWeight={400}>Master web development by making real-life projects. There are multiple paths for you to choose</Title>
-    <Input placeholder="Example" icon={<MdEmail style={{ width: '2.2rem', height: '2.2rem' }} />} />
+    <Input placeholder="Example" label="Email" icon={<MdEmail style={{ width: '2.2rem', height: '2.2rem' }} />} />
     <Input placeholder="Example" icon={<MdLock style={{ width: '2.2rem', height: '2.2rem' }} />} />
     <Button variant="primary">Login</Button>
     <hr className="divider" />
