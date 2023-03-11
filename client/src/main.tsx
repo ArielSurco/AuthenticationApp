cb4a87b95fe74ac8446cf0de4ea773e2aebbc39c
ArielSurco <arielsurcoa@gmail.com>
2023-03-11 01:40:47 -0300

    styles configuration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,12 @@
+// External
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Internal
 import App from './App';
+
+// Styles
+import 'styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
