94345e1dfd62877d96ffadc71c59dacd925dc5ea
ArielSurco <arielsurcoa@gmail.com>
2023-03-11 15:17:11 -0300

    disable require default prop in Card, and clean styles of button

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,10 +1,15 @@
 // Internal
-import { Title } from 'components/ui/atoms';
+import { Card, Title } from 'components/ui/atoms';
+
+// Assets
+import Logo from 'assets/devchallenges.svg';
 
 const Login = () => (
-  <div>
-    <Title>Login</Title>
-  </div>
+  <Card>
+    <img src={Logo} alt="logo" />
+    <Title level="h1" fontWeight={600}>Join thousands of learners from around the world</Title>
+    <Title level="h2" fontWeight={400}>Master web development by making real-life projects. There are multiple paths for you to choose</Title>
+  </Card>
 );
 
 export default Login;
